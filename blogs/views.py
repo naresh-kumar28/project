15eeaa41c6ba16e44f326e68791a9e2ccbf1a277
Naresh Kumar <nareshkumarsah780@gmail.com>
2026-05-24 17:46:05 +0530

    Add category-based post filtering and update templates
    - Implemented category context processor to fetch categories for navigation.
    - Updated home view to display featured and regular posts.
    - Added post_by_category view to filter posts by selected category.
    - Enhanced home and post_by_category templates for better layout and content display.
    - Modified settings to include category context processor and updated time zone.

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render, HttpResponse
+from .models import Blog, Category
 
 # Create your views here.
+
+def post_by_category(request, category_id):
+    posts = Blog.objects.filter(status='Published', category=category_id)
+    
+    # try:
+    #     category = Category.objects.get(pk=category_id)
+    # except:
+    #     # redirect the user to homepage
+    #     return redirect('home')
+    
+    category = get_object_or_404(Category, pk=category_id)
+
+
+    context = {
+        'posts' : posts,
+        'category' : category,
+    }
+
+    return render(request, 'post_by_category.html', context)
