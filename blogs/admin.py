e5ac7ca51f64106926953c536dd71be410a7b8da
Naresh Kumar <nareshkumarsah780@gmail.com>
2026-05-24 12:05:25 +0530

    Add Blog model with fields and admin configuration

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Blog
 
 # Register your models here.
 
@@ -11,3 +11,10 @@ admin.site.register(Category, CategoryAdmin)
 # @admin.register(Category)
 # class CategoryAdmin(admin.ModelAdmin):
 #     list_display = ('name', 'created_at', 'updated_at')
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'author', 'created_at', 'status', 'is_featured')
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ('id', 'title', 'category__category_name', 'status')
+    list_editable = ('is_featured','status')
